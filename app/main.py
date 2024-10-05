c48db31c253470eb167d3c3cc90f830ff001b6d4
tomrod10 <tomrod243@gmail.com>
2024-10-05 15:48:05 -0700

    fix typo and add comment

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    # adds instance of player classe to two groups
+    # adds instance of player classes to two groups
     Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -24,7 +24,7 @@ def main():
         screen.fill((0, 0, 0))  # fills frame with RGB/A color
         for item in updateable:  # updates player movement
             item.update(dt)
-        for item in drawable:
+        for item in drawable:  # draws sprites to screen
             item.draw(screen)
         pygame.display.flip()  # update contents of the whole display
         dt = clock.tick(60) / 1000  # limits frames to 60 fps
