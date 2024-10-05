cbba49596556b4ad1946e8b5b15cb5626270b384
tomrod10 <tomrod243@gmail.com>
2024-10-05 13:00:45 -0700

    added player logic to render on screen

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -14,6 +16,7 @@ def main():
                 return
 
         screen.fill((0, 0, 0))  # fills frame with RGB/A color
+        player.draw(screen)
         pygame.display.flip()  # update contents of the whole display
         dt = clock.tick(60) / 1000  # limits frames to 60 fps
 
